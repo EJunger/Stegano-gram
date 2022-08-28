1f1c75fd70eda51f30b767dab406e3956517769e
EJunger <emmajeane.junger@gmail.com>
2022-08-28 19:21:25 -0400

    Working on chunk type

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -6,62 +6,21 @@ use std::str::FromStr;
 // TODO:
 // double check resources page for better conversion methods
 
-// ancillary_bit - 0 (uppercase) = critical, 1 (lowercase) = ancillary.
-// private_bit - 0 (uppercase) = public, 1 (lowercase) = private.
-// reserved_bit - Must be 0 (uppercase) in files conforming to this version of PNG.
+// ancillary_bit    - 0 (uppercase) = critical, 1 (lowercase) = ancillary.
+// private_bit      - 0 (uppercase) = public, 1 (lowercase) = private.
+// reserved_bit     - Must be 0 (uppercase) in files conforming to this version of PNG.
 // safe_to_copy_bit - 0 (uppercase) = unsafe to copy, 1 (lowercase) = safe to copy.
 
 #[derive(Debug, PartialEq, Eq)]
-struct ChunkType {
-    ancillary_bit: char,
-    private_bit: char,
-    reserved_bit: char,
-    safe_to_copy_bit: char,
-}
-
-impl TryFrom<[u8; 4]> for ChunkType {
-    type Error = Box<dyn Error>; //NOTE: ParseIntError maybe
-
-    fn try_from(values: [u8; 4]) -> Result<Self, Self::Error> {
-        Ok(Self {
-            ancillary_bit: values[0] as char,
-            private_bit: values[1] as char,
-            reserved_bit: values[2] as char,
-            safe_to_copy_bit: values[3] as char,
-        })
-    }
-}
-
-impl FromStr for ChunkType {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.chars().any(|x| x.is_numeric()) {
-            return Err("!!Invalid chunk type!!");
-        }
-        let char_vec: Vec<char> = s.chars().collect();
-
-        Ok(Self {
-            ancillary_bit: char_vec[0],
-            private_bit: char_vec[1],
-            reserved_bit: char_vec[2],
-            safe_to_copy_bit: char_vec[3],
-        })
-    }
-}
-
-impl Display for ChunkType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}{}{}{}",
-            self.ancillary_bit, self.private_bit, self.reserved_bit, self.safe_to_copy_bit
-        )
-    }
+pub struct ChunkType {
+    pub ancillary_bit: char,
+    pub private_bit: char,
+    pub reserved_bit: char,
+    pub safe_to_copy_bit: char,
 }
 
 impl ChunkType {
-    fn bytes(&self) -> [u8; 4] {
+    pub fn bytes(&self) -> [u8; 4] {
         [
             self.ancillary_bit as u8,
             self.private_bit as u8,
@@ -89,6 +48,47 @@ impl ChunkType {
 
     fn is_safe_to_copy(&self) -> bool {
         self.safe_to_copy_bit.is_ascii_lowercase()
+    }
+}
+
+impl Display for ChunkType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}{}{}",
+            self.ancillary_bit, self.private_bit, self.reserved_bit, self.safe_to_copy_bit
+        )
+    }
+}
+
+impl FromStr for ChunkType {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.chars().any(|x| x.is_numeric()) {
+            return Err("!!Invalid chunk type!!");
+        }
+        let char_vec: Vec<char> = s.chars().collect();
+
+        Ok(Self {
+            ancillary_bit: char_vec[0],
+            private_bit: char_vec[1],
+            reserved_bit: char_vec[2],
+            safe_to_copy_bit: char_vec[3],
+        })
+    }
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = Box<dyn Error>; //NOTE: ParseIntError instead maybe
+
+    fn try_from(values: [u8; 4]) -> Result<Self, Self::Error> {
+        Ok(Self {
+            ancillary_bit: values[0] as char,
+            private_bit: values[1] as char,
+            reserved_bit: values[2] as char,
+            safe_to_copy_bit: values[3] as char,
+        })
     }
 }
 
